057126668418800634d1cf0ba71cf48aa3e49d01
julikorolko <korolko.yuliya@gmail.com>
2021-01-04 17:03:07 +0100

    changed version

---
+++ TravelAgencyCode/Client_booked.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektPO
+{public enum Status { adult,child}
+    public class Client_booked : AClient
+    {
+        
+        
+        Status status;
+        public Status Status { get => status; set => status = value; }
+        public Client_booked(string firstname, string surname, Status status)
+        {
+            this.Firstname = firstname;
+            this.Surname = surname;
+            this.Status = status;
+        }
+
+       
+
+        public override string ToString()
+        {
+            return Firstname + " " + Surname + "\n"
+                + "Status: " + Status;
+
+        }
+    }
+}
---
+++ TravelAgencyCode/IStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektPO
+{
+    interface IStore
+    {
+        void AddOffer(Offer offer);
+        void RemoveOffer(Offer offer);
+        void ClearOffers();
+
+    }
+}
