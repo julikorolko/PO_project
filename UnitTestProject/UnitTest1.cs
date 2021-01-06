6d7f5c896622392c0e0d59563f0c5746f7c9d89f
Karoltes <48433324+Karoltes@users.noreply.github.com>
2021-01-06 18:07:23 +0100

    Add files via upload

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,13 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using TravelAgency;
 namespace UnitTestProject
 {
 	[TestClass]
 	public class UnitTest1
 	{
+		Client c_test = new Client("Jan", "Kowalski", "ul. Wolska 34/6", "Warszawa 33-100",
+				   "504120236", "jankowalski201@gmail.com");
 		[TestMethod]
-		public void TestMethod1()
+		public void TestPhoneNumber()
 		{
+			string phone_num_test = "504120236";
+			Assert.AreEqual(c_test.Phone_num, phone_num_test);
+		}
+		[TestMethod]
+		public void TestMail()
+		{
+			string email_test = "jankowalski201@gmail.com";
+			Assert.AreEqual(c_test.Email, email_test);
 		}
 	}
 }
---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TravelAgency;
+namespace UnitTestProject
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		Client c_test = new Client("Jan", "Kowalski", "ul. Wolska 34/6", "Warszawa 33-100",
+				"504120203", "jan.kowalski34@gmail.com");
+		Offer o_test = new Offer("France - Nice", "Kraków", "Le Lazueu", 7, 1500, "21-01-2021", "28-01-2021");
+
+		[TestMethod]
+		public void TestBooking()
+		{
+			Booking b_test = new Booking(c_test, o_test, 3, Accomodation.Extra,
+				Flight_types.Standard, Board_types.Full, true);
+
+			Assert.IsNotNull(b_test.Accomodation);
+			Assert.IsNotNull(b_test.Flight);
+			Assert.IsNotNull(b_test.Board);
+		}
+
+		[TestMethod]
+		public void TestOfferDate()
+		{
+			DateTime arr = o_test.Date_arr;
+			DateTime dep = o_test.Date_dep;
+			int days = (arr - dep).Days;
+			Assert.AreEqual(days, o_test.Days);
+		}
+
+	}
+}
